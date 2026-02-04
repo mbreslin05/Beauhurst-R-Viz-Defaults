35cd17acaf0f4d31be2f225c9c56a1d4ec42ca4a
Michael Breslin <michael.breslin@beauhurst.com>
2026-02-04 17:05:46 +0000

    Add files via upload

---
+++ bhurst_viz_shortcuts.R
@@ -0,0 +1,40 @@
+
+
+
+# Packages ----------------------------------------------------------------
+
+
+library(ggplot2)
+
+
+
+# Theme -------------------------------------------------------------------
+
+
+theme_set(theme_minimal(base_family = 'Public Sans', base_size = 11) + 
+            theme(panel.grid.minor = element_blank(), 
+                  panel.grid.major.x = element_blank(), 
+                  panel.grid.major.y = element_blank(), 
+                  plot.title = element_text(face = 'bold', size = 13), 
+                  plot.title.position = 'plot',
+                  text = element_text(color = .black), 
+                  plot.background = element_rect(fill = 'white', color = 'white'), 
+                  axis.text = element_text(size = 9), 
+                  axis.title = element_text(size = 9)))
+
+
+
+# Colours -----------------------------------------------------------------
+
+.purple <- '#6B67DA'
+.black_purple <- '#211E52'
+.dark_purple <- '#38358E'
+.light_purple <- '#BBBAF6'
+.white_purple <- '#EAEAFF'
+.yellow <- '#FFB914'
+
+
+
+# Extra functions ---------------------------------------------------------
+
+
